e7777ad3371813e6d891893faafacb7ebbc252fe
Marina Nenova <marinanenova89@gmail.com>
2022-06-23 17:15:48 +0300

    Added code for 05_smartphone.py

--- OOP/Classes_and_objects/lab/05_smartphone.py
+++ OOP/Classes_and_objects/lab/05_smartphone.py
@@ -1,13 +1,30 @@
-class Vehicle:
-    def __init__(self, mileage, max_speed=150):
-        self.mileage = mileage
-        self.max_speed = max_speed
-        self.gadgets = []
+class Smartphone:
+    def __init__(self, memory):
+        self.memory = memory
+        self.apps = []
+        self.is_on = False
+
+    def power(self):
+        self.is_on = not self.is_on
+
+    def install(self, app, app_memory):
+        if self.memory >= app_memory:
+            if self.is_on:
+                self.apps.append(app)
+                self.memory -= app_memory
+                return f"Installing {app}"
+            return f"Turn on your phone to install {app}"
+        return f"Not enough memory to install {app}"
+
+    def status(self):
+        return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
 
 
-car = Vehicle(20)
-print(car.max_speed)
-print(car.mileage)
-print(car.gadgets)
-car.gadgets.append('Hudly Wireless')
-print(car.gadgets)
+
+smartphone = Smartphone(100)
+print(smartphone.install("Facebook", 60))
+smartphone.power()
+print(smartphone.install("Facebook", 60))
+print(smartphone.install("Messenger", 20))
+print(smartphone.install("Instagram", 40))
+print(smartphone.status())
