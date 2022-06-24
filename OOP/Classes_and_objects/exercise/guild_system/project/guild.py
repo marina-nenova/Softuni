432b103be3edc73c90f54f95b008ea545383763b
Marina Nenova <marinanenova89@gmail.com>
2022-06-24 14:02:25 +0300

    Added code for 06_guild_system.py

---
+++ OOP/Classes_and_objects/exercise/guild_system/project/guild.py
@@ -0,0 +1,31 @@
+from Classes_and_objects.exercise.guild_system.project.player import Player
+
+
+class Guild:
+    def __init__(self, name: str):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player: Player):
+        if player.guild == "Unaffiliated":
+            player.guild = self.name
+            self.players.append(player)
+            return f"Welcome player {player.name} to the guild {self.name}"
+        elif player.guild == self.name:
+            return f"Player {player.name} is already in the guild."
+        else:
+            return f"Player {player.name} is in another guild."
+
+    def kick_player(self, player_name: str):
+        for player in self.players:
+            if player.name == player_name:
+                player.guild = "Unaffiliated"
+                self.players.remove(player)
+                return f"Player {player_name} has been removed from the guild."
+        return f"Player {player_name} is not in the guild."
+
+    def guild_info(self):
+        output = f"Guild: {self.name}\n"
+        for player in self.players:
+            output += player.player_info()
+        return output
---
+++ OOP/Classes_and_objects/exercise/guild_system/project/player.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self, name: str, hp: int, mp: int):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = "Unaffiliated"
+
+    def add_skill(self, skill_name: str, mana_cost: int):
+        if skill_name not in self.skills:
+            self.skills[skill_name] = mana_cost
+            return f"Skill {skill_name} added to the collection of the player {self.name}"
+        return "Skill already added"
+
+    def player_info(self):
+        output = f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n"
+        for skill, mana_cost in self.skills.items():
+            output += f"==={skill} - {mana_cost}\n"
+        return output
