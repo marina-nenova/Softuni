a259e24f13e004d971cc9b09af61a02a37a8a4f0
Marina Nenova <marinanenova89@gmail.com>
2022-06-26 14:04:09 +0300

    reformatted code for 04_pizza_delivery.py

--- OOP/Classes_and_objects/exercise/04_pizza_delivery.py
+++ OOP/Classes_and_objects/exercise/04_pizza_delivery.py
@@ -16,12 +16,12 @@ class PizzaDelivery:
     def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
         if self.ordered:
             return f"Pizza {self.name} already prepared, and we can't make any changes!"
-        if ingredient in self.ingredients:
-            if quantity <= self.ingredients[ingredient]:
-                self.ingredients[ingredient] -= quantity
-                self.price -= quantity * price_per_quantity
+        if ingredient not in self.ingredients:
+            return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
+        if quantity > self.ingredients[ingredient]:
             return f"Please check again the desired quantity of {ingredient}!"
-        return f"Wrong ingredient selected! We do not use {ingredient} in {self.name}!"
+        self.ingredients[ingredient] -= quantity
+        self.price -= quantity * price_per_quantity
 
     def make_order(self):
         if not self.ordered:
