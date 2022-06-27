7b8fb3a9eda2c2a7c3b72ad4609a8d3901f00e79
Marina Nenova <marinanenova89@gmail.com>
2022-06-27 15:36:56 +0300

    added code for 05_hierarchical_inheritance.py

---
+++ OOP/Inheritance/lab/hierarchical_inheritance/05_hierarchical_inheritance.py
@@ -0,0 +1,10 @@
+from project.cat import Cat
+from project.dog import Dog
+
+felix = Cat()
+harry = Dog()
+
+print(felix.meow())
+print(felix.eat())
+print(harry.bark())
+print(harry.eat())
---
+++ OOP/Inheritance/lab/multilevel_inheritance/04_multilevel_inheritance.py
@@ -0,0 +1,6 @@
+from project.sports_car import SportsCar
+
+s_c = SportsCar()
+print(s_c.move())
+print(s_c.drive())
+print(s_c.race())
