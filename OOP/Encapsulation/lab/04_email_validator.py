4cb23022b293a440bf874ea5caf0df6bcb0d4f5a
Marina Nenova <marinanenova89@gmail.com>
2022-06-29 17:43:32 +0300

    Added logic for 04_email_validator.py

---
+++ OOP/Encapsulation/lab/04_email_validator.py
@@ -0,0 +1,29 @@
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        data = email.split("@")
+        name = data[0]
+        mail, domain = data[1].split(".")
+        return self.__is_name_valid(name) and self.__is_domain_valid(domain) and self.__is_mail_valid(mail)
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
+
