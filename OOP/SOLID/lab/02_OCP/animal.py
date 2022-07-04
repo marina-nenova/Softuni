601c6381c892cb97a3bd877dfee72c83769baca9
Marina Nenova <marinanenova89@gmail.com>
2022-07-04 19:24:53 +0300

    Added logic for 02_animals

--- OOP/SOLID/lab/02_OCP/animal.py
+++ OOP/SOLID/lab/02_OCP/animal.py
@@ -1,22 +1,31 @@
-class Animal:
-    def __init__(self, species):
-        self.species = species
+from abc import ABC, abstractmethod
 
-    def get_species(self):
-        return self.species
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+
+class Cat(Animal):
+    def make_sound(self):
+        return "meow"
+
+
+class Dog(Animal):
+    def make_sound(self):
+        return "woof"
+
+
+class Chicken(Animal):
+    def make_sound(self):
+        return "pew"
 
 
 def animal_sound(animals: list):
     for animal in animals:
-        if animal.species == 'cat':
-            print('meow')
-        elif animal.species == 'dog':
-            print('woof-woof')
+        print(animal.make_sound())
 
 
-animals = [Animal('cat'), Animal('dog')]
+animals = [Cat(), Dog(), Chicken()]
 animal_sound(animals)
-
-## добавете ново животно и рефакторирайте кода да работи без да се налага да се правят промени по него
-## при добавяне на нови животни
-# animals = [Animal('cat'), Animal('dog'), Animal('chicken')]
