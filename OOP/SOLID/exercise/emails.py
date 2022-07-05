0baa2e68d61cc6eca3ccd1e354f2cdca93bf541d
Marina Nenova <marinanenova89@gmail.com>
2022-07-05 16:59:28 +0300

    Added logic for emails.py

--- OOP/SOLID/exercise/emails.py
+++ OOP/SOLID/exercise/emails.py
@@ -41,24 +41,17 @@ class IEmail(ABC):
 
 class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
 
     def set_sender(self, sender):
-        if self.protocol == 'IM':
-            self.__sender = ''.join(["I'm ", sender])
-        else:
-            self.__sender = sender
+        self.__sender = sender
 
     def set_receiver(self, receiver):
-        if self.protocol == 'IM':
-            self.__receiver = ''.join(["I'm ", receiver])
-        else:
-            self.__receiver = receiver
+        self.__receiver = receiver
 
     def set_content(self, content):
         self.__content = content
@@ -70,7 +63,7 @@ class Email(IEmail):
         return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content.format())
 
 
-email = Email('IM', 'MyML')
+email = Email('IM')
 email.set_sender('qmal')
 email.set_receiver('james')
 email.set_content(EncryptedContent("Hello there!"))
