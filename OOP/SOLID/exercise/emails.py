4b01f0583eedccb9df0619f072957568013d9d1d
Marina Nenova <marinanenova89@gmail.com>
2022-07-05 16:56:38 +0300

    Added logic for emails.py

--- OOP/SOLID/exercise/emails.py
+++ OOP/SOLID/exercise/emails.py
@@ -1,8 +1,30 @@
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod, ABC
 
 
-class IEmail(object):
-    __metaclass__ = ABCMeta
+class IContent(ABC):
+    def __init__(self, text):
+        self.text = text
+
+    @abstractmethod
+    def format(self):
+        pass
+
+
+class MyMLContent(IContent):
+    def format(self):
+        return '<myML>\n' + self.text + '\n' + '</myML>'
+
+
+class EncryptedContent(IContent):
+    def format(self):
+        result = ""
+        for ch in self.text:
+            result += chr(ord(ch) + 5)
+
+        return result
+
+
+class IEmail(ABC):
 
     @abstractmethod
     def set_sender(self, sender):
@@ -15,6 +37,7 @@ class IEmail(object):
     @abstractmethod
     def set_content(self, content):
         pass
+
 
 class Email(IEmail):
 
@@ -38,21 +61,17 @@ class Email(IEmail):
             self.__receiver = receiver
 
     def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+        self.__content = content
 
     def __repr__(self):
 
         template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
 
-        return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
+        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content.format())
 
 
 email = Email('IM', 'MyML')
 email.set_sender('qmal')
 email.set_receiver('james')
-email.set_content('Hello, there!')
+email.set_content(EncryptedContent("Hello there!"))
 print(email)
-
