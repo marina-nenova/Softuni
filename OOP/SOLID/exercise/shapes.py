3479b7fc1ce0ab00a12a1e12c847e45c6363d52d
Marina Nenova <marinanenova89@gmail.com>
2022-07-05 16:39:11 +0300

    Added logic for shapes.py

--- OOP/SOLID/exercise/shapes.py
+++ OOP/SOLID/exercise/shapes.py
@@ -1,13 +1,42 @@
-class Rectangle:
+import math
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+
+class Rectangle(Shape):
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
+    def calculate_area(self):
+        return self.width * self.height
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return math.pi * self.radius ** 2
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def calculate_area(self):
+        return self.side * self.side
+
+
 class AreaCalculator:
 
     def __init__(self, shapes):
-
         assert isinstance(shapes, list), "`shapes` should be of type `list`."
         self.shapes = shapes
 
@@ -15,11 +44,10 @@ class AreaCalculator:
     def total_area(self):
         total = 0
         for shape in self.shapes:
-            total += shape.width * shape.height
-
+            total += shape.calculate_area()
         return total
 
 
-shapes = [Rectangle(2, 3), Rectangle(1, 6)]
+shapes = [Rectangle(2, 3), Rectangle(1, 6), Circle(5), Square(7)]
 calculator = AreaCalculator(shapes)
 print("The total area is: ", calculator.total_area)
