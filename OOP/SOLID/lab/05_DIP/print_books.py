9fdd34f3a652ff3b93caa73e2c89fd7b5ec07ec4
Marina Nenova <marinanenova89@gmail.com>
2022-07-05 15:12:17 +0300

    Added tlogic for 05_print_books

--- OOP/SOLID/lab/05_DIP/print_books.py
+++ OOP/SOLID/lab/05_DIP/print_books.py
@@ -1,15 +1,37 @@
 class Book:
-    def __init__(self, content: str):
+    def __init__(self, title, author, content: str):
+        self.title = title
+        self.author = author
         self.content = content
 
 
 class Formatter:
-    def format(self, book: Book) -> str:
+    @staticmethod
+    def format(book: Book) -> str:
         return book.content
 
 
+class PrePrintFlyer:
+    @staticmethod
+    def format(book):
+        return f"-------\n{book.title}\n-------\n{book.author}\n-------"
+
+
 class Printer:
+    def __init__(self, formatter):
+        self.formatter = formatter
+
     def get_book(self, book: Book):
-        formatter = Formatter()
-        formatted_book = formatter.format(book)
-        return formatted_book
+        return self.formatter.format(book)
+
+
+normal_formatter = Formatter()
+flier_formatter = PrePrintFlyer()
+
+b = Book("Title 1", "Author 1", "Some content")
+p = Printer(normal_formatter)
+p2 = Printer(flier_formatter)
+print(p.get_book(b))
+print()
+print()
+print(p2.get_book(b))
