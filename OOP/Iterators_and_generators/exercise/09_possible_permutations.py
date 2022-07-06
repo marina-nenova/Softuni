df078ba142e2302fe7d1f1747433d335f92eb255
Marina Nenova <marinanenova89@gmail.com>
2022-07-06 15:27:00 +0300

    Added logic for 09_possible_permutations.py

--- OOP/Iterators_and_generators/exercise/09_possible_permutations.py
+++ OOP/Iterators_and_generators/exercise/09_possible_permutations.py
@@ -1,5 +1,10 @@
+from itertools import permutations
+
+
 def possible_permutations(numbers):
-    pass
+    result = permutations(numbers)
+    for perm in result:
+        yield list(perm)
 
 
 [print(n) for n in possible_permutations([1, 2, 3])]
