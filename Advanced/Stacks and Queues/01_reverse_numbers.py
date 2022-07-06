78488b59639dd8cffc1b8cfd8574cf47477af9e4
Marina Nenova <marinanenova89@gmail.com>
2022-07-06 17:15:57 +0300

    Added folders for Stacks and Queues

--- Advanced/Stacks and Queues/01_reverse_numbers.py
+++
@@ -1,4 +0,0 @@
-numbers = input().split()
-
-while numbers:
-    print(numbers.pop(), end=" ")
--- Advanced/Stacks and Queues/01_reverse_strings.py
+++
@@ -1,8 +0,0 @@
-text = list(input())
-
-reverse_word = ""
-
-for char in range(len(text)):
-    reverse_word += text.pop()
-
-print(reverse_word)
