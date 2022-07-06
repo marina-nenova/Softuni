8f6e0bd70df8fecf6bf43f3407f7b85c12dc97ad
Marina Nenova <marinanenova89@gmail.com>
2022-07-06 16:06:34 +0300

    Added folders for Conditional statements lab, exercise and more exercise

--- Basics/Conditional statements/03_even_or_odd.py
+++
@@ -1,6 +0,0 @@
-number = int(input())
-
-if number % 2 == 0:
-    print("even")
-else:
-    print("odd")
--- Basics/Conditional statements/05_numbers_100_to_200.py
+++
@@ -1,6 +0,0 @@
-password = input()
-
-if password == "s3cr3t!P@ssw0rd":
-    print("Welcome")
-else:
-    print("Wrong password!")
