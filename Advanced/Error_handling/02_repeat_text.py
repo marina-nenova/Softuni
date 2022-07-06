d639b3c666589830579a530c36ea0f0e3fa883d2
Marina Nenova <marinanenova89@gmail.com>
2022-07-06 17:03:36 +0300

    Added folders for error handling

--- Advanced/Error_handling/02_repeat_text.py
+++
@@ -1,8 +0,0 @@
-text = input()
-
-
-try:
-    times = int(input())
-    print(text * times)
-except ValueError:
-    print("Variable times must be an integer")
--- Advanced/Error_handling/03_value_cannot_be_negative.py
+++
@@ -1,8 +0,0 @@
-class ValueCannotBeNegative(Exception):
-    pass
-
-
-while True:
-    num = int(input())
-    if num < 0:
-        raise ValueCannotBeNegative
