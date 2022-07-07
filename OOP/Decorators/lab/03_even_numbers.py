c423b4ed3a921e438a982e66360e5e7d4cda3567
Marina Nenova <marinanenova89@gmail.com>
2022-07-07 15:16:04 +0300

    Added logic for 03_even_numbers.py

---
+++ OOP/Decorators/lab/03_even_numbers.py
@@ -0,0 +1,12 @@
+def even_numbers(function):
+    def wrapper(numbers):
+        return [num for num in numbers if num % 2 == 0]
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5, 8, 12, 24]))
