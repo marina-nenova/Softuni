618e2b5ba7b9f36edc69428d231ea667867f6cc0
Marina Nenova <marinanenova89@gmail.com>
2022-07-08 16:12:37 +0300

    Added logic for 08_execution_time.py

---
+++ OOP/Decorators/exercise/08_execution_time.py
@@ -0,0 +1,43 @@
+from time import time
+
+
+def exec_time(func):
+    def wrapper(*args):
+        start = time()
+        func(*args)
+        end = time()
+        return end - start
+
+    return wrapper
+
+
+@exec_time
+def loop(start, end):
+    total = 0
+    for x in range(start, end):
+        total += x
+    return total
+
+
+print(loop(1, 10000000))
+
+
+@exec_time
+def concatenate(strings):
+    result = ""
+    for string in strings:
+        result += string
+    return result
+
+
+print(concatenate(["a" for i in range(1000000)]))
+
+
+@exec_time
+def loop():
+    count = 0
+    for i in range(1, 9999999):
+        count += 1
+
+
+print(loop())
