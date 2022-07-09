bb49e1fc37203e8b85ef91e4e2205aa5f3127a0e
Marina Nenova <marinanenova89@gmail.com>
2022-07-09 18:12:19 +0300

    Added logic for 01_test_worker.py

--- OOP/Unit_testing/lab/01_test_worker.py
+++ OOP/Unit_testing/lab/01_test_worker.py
@@ -18,3 +18,65 @@ class Worker:
 
     def get_info(self):
         return f'{self.name} has saved {self.money} money.'
+
+
+from unittest import TestCase, main
+
+
+class WorkerTests(TestCase):
+    def test_worker_initialized_correctly(self):
+        worker = Worker("Test", 100, 10)
+        self.assertEqual("Test", worker.name)
+        self.assertEqual(100, worker.salary)
+        self.assertEqual(10, worker.energy)
+
+    def test_worker_energy_decreased_after_working(self):
+        worker = Worker("Test", 100, 10)
+        self.assertEqual(10, worker.energy)
+        worker.work()
+        self.assertEqual(9, worker.energy)
+        worker.work()
+        self.assertEqual(8, worker.energy)
+
+    def test_worker_tries_to_work_with_zero_energy(self):
+        worker = Worker("Test", 100, 0)
+        self.assertEqual(0, worker.energy)
+
+        with self.assertRaises(Exception) as ex:
+            worker.work()
+        self.assertEqual('Not enough energy.', str(ex.exception))
+
+    def test_worker_tries_to_work_with_negative_energy(self):
+        worker = Worker("Test", 100, -1)
+        self.assertEqual(-1, worker.energy)
+
+        with self.assertRaises(Exception) as ex:
+            worker.work()
+        self.assertEqual('Not enough energy.', str(ex.exception))
+
+    def test_worker_money_increased_after_working(self):
+        worker = Worker("Test", 100, 10)
+        self.assertEqual(0, worker.money)
+
+        worker.work()
+        self.assertEqual(worker.money, 100)
+
+        worker.work()
+        self.assertEqual(worker.money, 200)
+
+    def test_worker_energy_increased_after_resting(self):
+        worker = Worker("Test", 100, 10)
+        self.assertEqual(10, worker.energy)
+        worker.rest()
+        self.assertEqual(11, worker.energy)
+        worker.rest()
+        self.assertEqual(12, worker.energy)
+
+    def test_get_info(self):
+        worker = Worker("Test", 100, 10)
+        result = worker.get_info()
+        self.assertEqual('Test has saved 0 money.', result)
+
+
+if __name__ == "__main__":
+    main()
