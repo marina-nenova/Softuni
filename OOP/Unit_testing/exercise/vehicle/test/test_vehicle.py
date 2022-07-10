1c674b005e61a74245bc2f9a584d242e17c21ae6
Marina Nenova <marinanenova89@gmail.com>
2022-07-10 16:51:47 +0300

    Added setup method for test vehicle

--- OOP/Unit_testing/exercise/vehicle/test/test_vehicle.py
+++ OOP/Unit_testing/exercise/vehicle/test/test_vehicle.py
@@ -4,76 +4,52 @@ from project.vehicle import Vehicle
 
 
 class VehicleTests(TestCase):
-    def test_vehicle_fuel_initialized_correctly_with_integer_value(self):
+    def setUp(self) -> None:
+        self.car = Vehicle(40, 120)
+
+    def test_vehicle_init(self):
         car = Vehicle(40, 120)
         self.assertEqual(40, car.fuel)
-
-    def test_vehicle_fuel_initialized_correctly_with_float_value(self):
-        car = Vehicle(40.5, 120)
-        self.assertEqual(40.5, car.fuel)
-
-    def test_vehicle_fuel_capacity_initialized_correctly_with_integer_value(self):
-        car = Vehicle(40, 120)
         self.assertEqual(40, car.capacity)
-
-    def test_vehicle_fuel_capacity_initialized_correctly_with_float_value(self):
-        car = Vehicle(40.5, 120)
-        self.assertEqual(40.5, car.capacity)
-
-    def test_vehicle_horse_power_initialized_correctly_with_integer_value(self):
-        car = Vehicle(40, 120)
         self.assertEqual(120, car.horse_power)
-
-    def test_vehicle_horse_power_initialized_correctly_with_float_value(self):
-        car = Vehicle(40.5, 120.5)
-        self.assertEqual(120.5, car.horse_power)
-
-    def test_vehicle_fuel_consumption_initialized_correctly_with_default_value(self):
-        car = Vehicle(40.5, 120.5)
-        self.assertEqual(1.25, car.DEFAULT_FUEL_CONSUMPTION)
-        self.assertEqual(1.25, car.fuel_consumption)
+        self.assertEqual(Vehicle.DEFAULT_FUEL_CONSUMPTION, car.fuel_consumption)
 
     def test_drive_vehicle_with_enough_fuel_fuel_amount_decremented(self):
-        car = Vehicle(40, 120)
-        car.drive(20)
-        self.assertEqual(15 ,car.fuel)
+        distance = 20
+        burned_fuel = distance * self.car.fuel_consumption
+        expected = self.car.fuel - burned_fuel
+        self.car.drive(distance)
+        self.assertEqual(expected, self.car.fuel)
 
-        car.drive(10)
-        self.assertEqual(2.5, car.fuel)
+        distance = 10
+        burned_fuel = distance * self.car.fuel_consumption
+        expected = self.car.fuel - burned_fuel
+        self.car.drive(distance)
+        self.assertEqual(expected, self.car.fuel)
 
     def test_drive_vehicle_with_not_enough_fuel_raises(self):
-        car = Vehicle(40, 120)
-
         with self.assertRaises(Exception) as ex:
-            car.drive(40)
+            self.car.drive(self.car.fuel)
         self.assertEqual("Not enough fuel", str(ex.exception))
 
     def test_refuel_vehicle_with_fuel_less_than_capacity(self):
-        car = Vehicle(40, 120)
-        car.drive(20)
-        self.assertEqual(40, car.capacity)
-        self.assertEqual(15, car.fuel)
+        self.car.fuel = 20
 
-        car.refuel(15)
-        self.assertEqual(30, car.fuel)
+        self.car.refuel(15)
+        self.assertEqual(35, self.car.fuel)
 
-        car.refuel(5)
-        self.assertEqual(35, car.fuel)
+        self.car.refuel(5)
+        self.assertEqual(40, self.car.fuel)
 
     def test_refuel_vehicle_with_fuel_more_than_capacity_raises(self):
-        car = Vehicle(40, 120)
-        car.drive(20)
-        self.assertEqual(40, car.capacity)
-        self.assertEqual(15, car.fuel)
-
         with self.assertRaises(Exception) as ex:
-            car.refuel(30)
+            self.car.refuel(30)
         self.assertEqual("Too much fuel", str(ex.exception))
 
     def test_vehicle_string_representation_returns_correctly(self):
-        car = Vehicle(40, 120)
-        result = str(car)
-        self.assertEqual("The vehicle has 120 horse power with 40 fuel left and 1.25 fuel consumption", result)
+        result = str(self.car)
+        expected = f"The vehicle has {self.car.horse_power} horse power with {self.car.fuel} fuel left and {self.car.fuel_consumption} fuel consumption"
+        self.assertEqual(expected, result)
 
 
 if __name__ == "__main__":
