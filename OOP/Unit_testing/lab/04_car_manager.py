6cb7beaa72b87cdbde93f16630480b68b98f2501
Marina Nenova <marinanenova89@gmail.com>
2022-07-10 14:44:30 +0300

    Added logic for 04_car_manager.py

--- OOP/Unit_testing/lab/04_car_manager.py
+++ OOP/Unit_testing/lab/04_car_manager.py
@@ -72,15 +72,109 @@ class Car:
         self.__fuel_amount -= needed
 
 
-car = Car("a", "b", 1, 4)
-car.make = ""
-print(car)
-
-
 from unittest import TestCase, main
 
+
 class CarTests(TestCase):
-    pass
+    def test_make_is_initialized_correctly_with_valid_data(self):
+        car = Car("a", "b", 1, 4)
+        self.assertEqual("a", car.make)
+
+    def test_make_is_initialized_with_invalid_data_raises(self):
+        with self.assertRaises(Exception) as ex:
+            car = Car("", "b", 1, 4)
+        self.assertEqual("Make cannot be null or empty!", str(ex.exception))
+
+    def test_model_is_initialized_correctly_with_valid_data(self):
+        car = Car("a", "b", 1, 4)
+        self.assertEqual("b", car.model)
+
+    def test_model_is_initialized_with_invalid_data_raises(self):
+        with self.assertRaises(Exception) as ex:
+            car = Car("a", "", 1, 4)
+        self.assertEqual("Model cannot be null or empty!", str(ex.exception))
+
+    def test_fuel_consumption_is_initialized_correctly_with_valid_data(self):
+        car = Car("a", "b", 1, 4)
+        self.assertEqual(1, car.fuel_consumption)
+
+    def test_fuel_consumption_is_initialized_with_zero_or_negative_value_raises(self):
+        with self.assertRaises(Exception) as ex:
+            car = Car("a", "b", 0, 4)
+        self.assertEqual("Fuel consumption cannot be zero or negative!", str(ex.exception))
+
+        with self.assertRaises(Exception) as ex:
+            car = Car("a", "b", -5, 4)
+        self.assertEqual("Fuel consumption cannot be zero or negative!", str(ex.exception))
+
+    def test_fuel_capacity_is_initialized_correctly_with_valid_data(self):
+        car = Car("a", "b", 1, 4)
+        self.assertEqual(4, car.fuel_capacity)
+
+    def test_fuel_capacity_is_initialized_with_zero_or_negative_value_raises(self):
+        with self.assertRaises(Exception) as ex:
+            car = Car("a", "b", 1, 0)
+        self.assertEqual("Fuel capacity cannot be zero or negative!", str(ex.exception))
+
+        with self.assertRaises(Exception) as ex:
+            car = Car("a", "b", 1, -5)
+        self.assertEqual("Fuel capacity cannot be zero or negative!", str(ex.exception))
+
+    def test_fuel_amount_is_initialized_correctly(self):
+        car = Car("a", "b", 1, 4)
+        self.assertEqual(0, car.fuel_amount)
+
+    def test_fuel_amount_assigned_new_value_correctly_with_valid_data(self):
+        car = Car("a", "b", 1, 4)
+        self.assertEqual(0, car.fuel_amount)
+        car.fuel_amount = 10
+        self.assertEqual(10, car.fuel_amount)
+
+    def test_fuel_amount_assigned_new_value_with_invalid_data_raises(self):
+        car = Car("a", "b", 1, 4)
+        self.assertEqual(0, car.fuel_amount)
+        with self.assertRaises(Exception) as ex:
+            car.fuel_amount = -10
+        self.assertEqual("Fuel amount cannot be negative!", str(ex.exception))
+
+    def test_car_refuel_with_negative_or_zero_amount_raises(self):
+        car = Car("a", "b", 1, 4)
+
+        with self.assertRaises(Exception) as ex:
+            car.refuel(-10)
+        self.assertEqual("Fuel amount cannot be zero or negative!", str(ex.exception))
+
+        with self.assertRaises(Exception) as ex:
+            car.refuel(0)
+        self.assertEqual("Fuel amount cannot be zero or negative!", str(ex.exception))
+
+    def test_car_refuel_with_positive_amount_less_than_fuel_capacity(self):
+        car = Car("a", "b", 1, 40)
+        self.assertEqual(0, car.fuel_amount)
+        car.refuel(30)
+        self.assertEqual(30, car.fuel_amount)
+
+    def test_car_refuel_with_positive_amount_more_than_fuel_capacity(self):
+        car = Car("a", "b", 1, 40)
+        self.assertEqual(0, car.fuel_amount)
+        car.refuel(50)
+        self.assertEqual(40, car.fuel_amount)
+
+    def test_car_drives_with_enough_fuel_for_provided_distance(self):
+        car = Car("a", "b", 6.5, 40)
+        car.refuel(30)
+        self.assertEqual(30, car.fuel_amount)
+        car.drive(400)
+        self.assertEqual(4, car.fuel_amount)
+
+    def test_car_drives_with_not_enough_fuel_amount_for_provided_distance_raises(self):
+        car = Car("a", "b", 6.5, 40)
+        car.refuel(30)
+        self.assertEqual(30, car.fuel_amount)
+
+        with self.assertRaises(Exception) as ex:
+            car.drive(600)
+        self.assertEqual("You don't have enough fuel to drive!", str(ex.exception))
 
 
 if __name__ == "__main__":
