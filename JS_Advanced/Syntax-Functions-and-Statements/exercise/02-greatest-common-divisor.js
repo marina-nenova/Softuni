2b97f14a715cb0bb57a26440c32fb6008f25c242
Marina Nenova <marinanenova89@gmail.com>
2022-08-23 13:22:06 +0300

    Added logic for 02-greatest-common-divisor.js

---
+++ JS_Advanced/Syntax-Functions-and-Statements/exercise/02-greatest-common-divisor.js
@@ -0,0 +1,15 @@
+function solve(a, b) {
+    let firstNumber = Number(a);
+    let secondNumber = Number(b);
+
+    while (firstNumber !== secondNumber) {
+        if (firstNumber > secondNumber) {
+            firstNumber -= secondNumber;
+        } else {
+            secondNumber -= firstNumber;
+        }
+    }console.log(firstNumber);
+
+
+}
+solve(15, 5)
