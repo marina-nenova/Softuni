9acab6f98c8af0bdcbf500441c5708bc4ab4e5c2
Marina Nenova <marinanenova89@gmail.com>
2022-08-23 13:12:06 +0300

    Added logic for 04-previous-day.js

---
+++ JS_Advanced/Syntax-Functions-and-Statements/exercise/04-previous-day.js
@@ -0,0 +1,7 @@
+function previousDay(year, month, day) {
+    let dateInput = `${year}-${month}-${day}`;
+    let date = new Date(dateInput);
+    date.setDate(date.getDate()-1);
+    console.log(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
+}
+previousDay(2016, 1, 1);
