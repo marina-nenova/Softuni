92195ce712e623aa5249b83ca98bb12ec2e9a2cb
Marina Nenova <marinanenova89@gmail.com>
2022-08-25 16:59:27 +0300

    Added logic for 09-biggest-element.js

---
+++ JS_Advanced/Arrays-and-nested-arrays/lab/09-biggest-element.js
@@ -0,0 +1,21 @@
+function solve(array) {
+    let maxNum = 0;
+
+    for (let index = 0; index < array.length; index++) {
+        let currentArr = array[index];
+        let currentMaxNum = Math.max(...currentArr);
+
+        if (currentMaxNum >= maxNum) {
+            maxNum = currentMaxNum;
+        }
+
+    }
+    return maxNum
+}
+
+result = solve([[3, 5, 7, 12],
+[-1, 4, 33, '2'],
+[8, 3, 0, 4]]
+
+)
+console.log(result);
