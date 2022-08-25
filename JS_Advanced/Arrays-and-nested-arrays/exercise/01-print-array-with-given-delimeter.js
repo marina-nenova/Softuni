684d2343f423d9fd35474df10158ee382b68a4d6
Marina Nenova <marinanenova89@gmail.com>
2022-08-25 18:41:20 +0300

    Added logic for 01-print-array-with-given-delimeter.js

---
+++ JS_Advanced/Arrays-and-nested-arrays/exercise/01-print-array-with-given-delimeter.js
@@ -0,0 +1,20 @@
+function solve(array, delimeter) {
+    console.log(array.join(delimeter));
+}
+
+solve(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'
+)
+
+solve(['How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_'
+)
