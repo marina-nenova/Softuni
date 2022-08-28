a31e8274896efb9f45db5c8a3fbd2c6158064240
Marina Nenova <marinanenova89@gmail.com>
2022-08-28 15:17:08 +0300

    Added logic for 05-lowest-prices-in-cities.js

---
+++ JS_Advanced/Objects-and-composition/exercise/05-lowest-prices-in-cities.js
@@ -0,0 +1,30 @@
+function solve(array) {
+    let products = new Map();
+    for (let sentance of array) {
+        let [town, product, price] = sentance.split(" | ");
+        if (!products.has(product)) {
+            products.set(product, new Map());
+        }
+        products.get(product).set(town, Number(price));
+    }
+    for (let [key, value] of products) {
+        let lowest = ([...value].reduce(function (a, b) {
+            if (a[1] < b[1]) {
+                return a;
+            } else if (a[1] > b[1]) {
+                return b;
+            }
+            return a;
+        }));
+        console.log(`${key} -> ${lowest[1]} (${lowest[0]})`);
+    }
+}
+
+solve(['Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10']
+)
