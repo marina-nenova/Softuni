612831f2c59e70f12dbee3d9ce5ee51390c6180f
Marina Nenova <marinanenova89@gmail.com>
2022-08-29 15:01:08 +0300

    Added logic for 08-rectangle.js

---
+++ JS_Advanced/Objects-and-composition/exercise/08-rectangle.js
@@ -0,0 +1,17 @@
+function rectangle(width, height, color) {
+    let rectangle = {
+        width: width,
+        height: height,
+        color: color[0].toUpperCase() + color.slice(1),
+        calcArea: function () {
+            return this.width * this.height;
+        }
+    }
+    return rectangle;
+}
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
