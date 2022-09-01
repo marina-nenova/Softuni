169cc1b6aaa0915563485a58ddef00383a6f8b8f
Marina Nenova <marinanenova89@gmail.com>
2022-09-01 20:23:57 +0300

    Added logic for 03.SumNumbers.js

--- JS_Advanced/DOM-introduction/lab/03.Sum Numbers/SumNumbers.js
+++ JS_Advanced/DOM-introduction/lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,11 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let num1 = document.getElementById('num1');
+    let num2 = document.getElementById('num2');
+
+    console.log(typeof num1);
+
+    let sum = Number(num1.value) + Number(num2.value);
+
+    let resultElement = document.getElementById('sum')
+    resultElement.value = sum;
 }
