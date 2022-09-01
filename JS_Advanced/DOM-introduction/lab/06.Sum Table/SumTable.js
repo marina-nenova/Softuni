652765fff7ef3588fd8dd8332845735579f22173
Marina Nenova <marinanenova89@gmail.com>
2022-09-01 21:04:35 +0300

    Added logic for 06.SumTable.js

--- JS_Advanced/DOM-introduction/lab/06.Sum Table/SumTable.js
+++ JS_Advanced/DOM-introduction/lab/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    let rowElements = document.querySelectorAll('tr td:nth-of-type(2)');
 
+    let sum = Array.from(rowElements).reduce((a, x) => {
+        let currentElement = Number(x.textContent) || 0;
+        return a + currentElement;
+    }, 0)
+
+    let sumElement = document.getElementById('sum')
+    sumElement.textContent = sum
 }
