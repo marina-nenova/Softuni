736f59c5d80ecb83da16541643b419abe36d53a5
Marina Nenova <marinanenova89@gmail.com>
2022-09-01 20:52:54 +0300

    Added logic for 05.ColorizeTable.js

--- JS_Advanced/DOM-introduction/lab/05.Colorize Table/ColorizeTable.js
+++ JS_Advanced/DOM-introduction/lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,16 @@
 function colorize() {
-    // TODO
+    // let rowElements = document.querySelectorAll('tr:nth-of-type(2n) td');
+
+    // rowElements.forEach(x => {
+    //     x.style.backgroundColor = 'teal';
+    // })
+
+    let rowElements = document.getElementsByTagName('tr');
+    let rows = Array.from(rowElements);
+
+    rows.forEach((x, i) => {
+        if (i%2 !== 0) {
+            x.style.backgroundColor = 'teal';
+        }
+    })
 }
