c2ed6f00eae4fb40e96e93a6f831f33b9cc1cb8f
Marina Nenova <marinanenova89@gmail.com>
2022-09-01 20:53:22 +0300

    Refactorred code for 05.ColorizeTable.js

--- JS_Advanced/DOM-introduction/lab/05.Colorize Table/ColorizeTable.js
+++ JS_Advanced/DOM-introduction/lab/05.Colorize Table/ColorizeTable.js
@@ -9,7 +9,7 @@ function colorize() {
     let rows = Array.from(rowElements);
 
     rows.forEach((x, i) => {
-        if (i%2 !== 0) {
+        if (i % 2 !== 0) {
             x.style.backgroundColor = 'teal';
         }
     })
