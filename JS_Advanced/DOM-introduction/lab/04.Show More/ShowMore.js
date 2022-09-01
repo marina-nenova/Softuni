e28dbd388326147053206686f61e14e8658838e5
Marina Nenova <marinanenova89@gmail.com>
2022-09-01 20:28:19 +0300

    Added logic for 04.ShowMore.js

--- JS_Advanced/DOM-introduction/lab/04.Show More/ShowMore.js
+++ JS_Advanced/DOM-introduction/lab/04.Show More/ShowMore.js
@@ -1,3 +1,7 @@
 function showText() {
-    // TODO
+    let readMore = document.getElementById('more');
+    let textElement = document.getElementById('text');
+
+    readMore.style.display = 'none';
+    textElement.style.display = 'inline';
 }
