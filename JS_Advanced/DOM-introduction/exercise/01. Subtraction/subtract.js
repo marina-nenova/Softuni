7d343f8ebe2991f8a4450c7650783d988213a1fe
Marina Nenova <marinanenova89@gmail.com>
2022-09-01 21:58:32 +0300

    Added logic for 01.subtract.js

--- JS_Advanced/DOM-introduction/exercise/01. Subtraction/subtract.js
+++ JS_Advanced/DOM-introduction/exercise/01. Subtraction/subtract.js
@@ -1,3 +1,12 @@
 function subtract() {
-    console.log('TODO:...');
+    let num1 = document.getElementById("firstNumber").value;
+    let num2 = document.getElementById("secondNumber").value;
+
+    let firstNumber = Number(num1);
+    let secondNumber = Number(num2);
+
+    let result = firstNumber - secondNumber;
+
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = result;
 }
