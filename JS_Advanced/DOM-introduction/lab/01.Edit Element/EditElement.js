4542e2e97851779b525c6055a051c826c83a8ea8
Marina Nenova <marinanenova89@gmail.com>
2022-09-01 18:26:20 +0300

    Added logic for 01.EditElement.js

---
+++ JS_Advanced/DOM-introduction/lab/01.Edit Element/EditElement.js
@@ -0,0 +1,7 @@
+function editElement(element, match, replacement) {
+    let textElement = element.textContent;
+    while (textElement.includes(match)) {
+        textElement = textElement.replace(match, replacement);
+    }
+    element.textContent= textElement;
+}
