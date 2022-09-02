d5f5b7cb55a29cd95f50eade9a7f71a15f2fc524
Marina Nenova <marinanenova89@gmail.com>
2022-09-02 13:19:20 +0300

    Added logic for 03.accordion.js

--- JS_Advanced/DOM-introduction/exercise/03. Accordion/accordion.js
+++ JS_Advanced/DOM-introduction/exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    let textElement = document.getElementsByClassName('button')[0];
+    let moreInfo = document.getElementById('extra');
+    if (textElement.textContent === 'More') {
+        moreInfo.style.display = 'block';
+        textElement.textContent = 'Less'
+    } else if (textElement.textContent === 'Less') {
+        moreInfo.style.display = 'none';
+        textElement.textContent = 'More'
+    }
 }
