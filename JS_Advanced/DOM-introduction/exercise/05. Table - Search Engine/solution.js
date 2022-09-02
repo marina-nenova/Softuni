b7d47cffd3e2d9391707fc528e53f5de7791f200
Marina Nenova <marinanenova89@gmail.com>
2022-09-02 15:15:43 +0300

    Added logic for 05.Table - Search Engine

--- JS_Advanced/DOM-introduction/exercise/05. Table - Search Engine/solution.js
+++ JS_Advanced/DOM-introduction/exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,16 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let input = document.getElementById('searchField');
+   let rows = document.querySelectorAll('tbody tr');
 
    function onClick() {
-      //   TODO:
 
+      for (let row of rows) {
+         row.classList.remove('select');
+         if (input.value !== '' && row.innerHTML.includes(input.value)) {
+            row.className = 'select';
+         }
+      }
+      input.value = '';
    }
 }
