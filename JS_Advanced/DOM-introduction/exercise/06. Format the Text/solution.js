a660bcbee851118d755b8c86b4124f66c97ffdea
Marina Nenova <marinanenova89@gmail.com>
2022-09-02 15:41:33 +0300

    Added logic for 06.Format the Text

--- JS_Advanced/DOM-introduction/exercise/06. Format the Text/solution.js
+++ JS_Advanced/DOM-introduction/exercise/06. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input');
+
+  let inputArr = input.value.split('.').filter(s => s !== '');
+
+  let resultDiv = document.getElementById('output');
+
+  while (inputArr.length > 0) {
+    let text = inputArr.splice(0, 3).join(". ") + '.';
+    let p = document.createElement('p');
+    p.textContent = text;
+    resultDiv.appendChild(p);
+  }
 }
