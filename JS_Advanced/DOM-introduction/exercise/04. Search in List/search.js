42609195420897a3d17b3cf4a88092d108645f91
Marina Nenova <marinanenova89@gmail.com>
2022-09-02 14:54:34 +0300

    Added logic for 04.Search in list

--- JS_Advanced/DOM-introduction/exercise/04. Search in List/search.js
+++ JS_Advanced/DOM-introduction/exercise/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let userWord = document.getElementById('searchText').value;
+   let townsElements = Array.from(document.querySelectorAll('#towns li'));
+   let matchesElement = document.getElementById('result');
+
+   let count = 0;
+
+   for (let town of townsElements) {
+      if (town.textContent.includes(userWord)) {
+         count++;
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+      } else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+   matchesElement.textContent = `${count} matches found`
 }
