ae4c3f9f8eabc02478283b9b58ac257f8b53521c
Marina Nenova <marinanenova89@gmail.com>
2022-09-03 11:43:21 +0300

    Added logic for 08.Generate Report

--- JS_Advanced/DOM-introduction/exercise/08. Generate Report/report.js
+++ JS_Advanced/DOM-introduction/exercise/08. Generate Report/report.js
@@ -1,3 +1,34 @@
 function generateReport() {
-    //TODO
+    let checkBoxes = document.querySelectorAll('input')
+
+    let cbIndices = [];
+
+    for (let index = 0; index < checkBoxes.length; index++) {
+        const element = checkBoxes[index];
+        if (element.checked) {
+            cbIndices.push(index);
+        }
+    }
+    let rows = document.getElementsByTagName('tr');
+    rowsArr = Array.from(rows);
+    rowsArr.shift();
+
+    let result = [];
+
+
+    for (let row of rowsArr) {
+        let info = {};
+        for (let index of cbIndices) {
+            let name = checkBoxes[index].name;
+            let rowIinfo = row.children[index].textContent;
+            info[name] = rowIinfo;
+        }
+        result.push(info)
+
+    }
+    report = JSON.stringify(result);
+
+    let outputElement = document.getElementById('output');
+    outputElement.textContent = report;
+
 }
