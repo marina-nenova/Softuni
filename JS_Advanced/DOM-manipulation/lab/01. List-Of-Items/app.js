651f40c5ffb70971419b83e5e1124e92d0ddb754
Marina Nenova <marinanenova89@gmail.com>
2022-09-05 13:49:52 +0300

    Added logic for 01. List-Of-Items

--- JS_Advanced/DOM-manipulation/lab/01. List-Of-Items/app.js
+++ JS_Advanced/DOM-manipulation/lab/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemsElement = document.getElementById('items');
+    let inputTextElement = document.getElementById('newItemText');
+
+    let newLiElement = document.createElement('li');
+    newLiElement.textContent = inputTextElement.value;
+
+    itemsElement.appendChild(newLiElement);
+    inputTextElement.value = '';
 }
