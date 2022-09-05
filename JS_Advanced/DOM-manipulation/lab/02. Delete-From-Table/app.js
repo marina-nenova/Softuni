4ef73a573a0b8eaae71906b0d0e45138ae71840f
Marina Nenova <marinanenova89@gmail.com>
2022-09-05 14:05:28 +0300

    Added logic for 02. Delete-From-Table

--- JS_Advanced/DOM-manipulation/lab/02. Delete-From-Table/app.js
+++ JS_Advanced/DOM-manipulation/lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emailInputElement = document.querySelector('input[name="email"]');
+    let resultElement = document.getElementById('result');
+
+    let emailCellElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    let emailElements = Array.from(emailCellElements);
+
+    let targetElement = emailElements.find(x => x.textContent == emailInputElement.value)
+
+    if (targetElement) {
+        targetElement.parentNode.remove()
+        resultElement.textContent = 'Deleted.'
+    } else {
+        resultElement.textContent = 'Not found.'
+    }
+    emailInputElement.value = ''
 }
