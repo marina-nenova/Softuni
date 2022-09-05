38526de424a0d02c29a766f5fd216cfc4146642b
Marina Nenova <marinanenova89@gmail.com>
2022-09-05 15:07:07 +0300

    Added logic for 05.Highlight-Active

--- JS_Advanced/DOM-manipulation/lab/05. Highlight-Active/app.js
+++ JS_Advanced/DOM-manipulation/lab/05. Highlight-Active/app.js
@@ -1,3 +1,24 @@
 function focused() {
-    console.log('TODO:...');
+    let mainDiv = document.getElementsByTagName("div")[0];
+    let divArray = Array.from(mainDiv.getElementsByTagName('input'));
+    
+    divArray.forEach(element => {
+        element.addEventListener("focus", focus);
+    });
+
+    divArray.forEach(element => {
+        element.addEventListener("blur", focusLost);
+    });
+
+
+    function focus(e) {
+        let parent = e.target.parentNode;
+        parent.classList.add("focused");
+    }
+
+    function focusLost(e) {
+        let parent = e.target.parentNode;
+        parent.classList.remove("focused");
+    }
+
 }
