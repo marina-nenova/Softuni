22fdccc9958ed2c7c39c6fc3c2c9c80d9f74e720
Marina Nenova <marinanenova89@gmail.com>
2022-09-05 14:23:53 +0300

    Added logic for 03.Add-Delete

--- JS_Advanced/DOM-manipulation/lab/03. Add-Delete/app.js
+++ JS_Advanced/DOM-manipulation/lab/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    let itemsElement = document.getElementById('items');
+    let inputTextElement = document.getElementById('newItemText')
+
+    let newLiElement = document.createElement('li');
+    newLiElement.textContent = inputTextElement.value;
+
+    let deleteLinkElement = document.createElement('a')
+    deleteLinkElement.href = '#';
+    deleteLinkElement.textContent = '[Delete]'
+
+    deleteLinkElement.addEventListener('click', function (el) {
+        el.currentTarget.parentNode.remove();
+    })
+
+    newLiElement.appendChild(deleteLinkElement);
+
+    itemsElement.appendChild(newLiElement);
+    inputTextElement.value = '';
+
 }
