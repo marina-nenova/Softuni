285b35663d3ddfae0f1484f2000f6d1b6b8be426
Marina Nenova <marinanenova89@gmail.com>
2022-09-07 12:39:45 +0300

    Added logic for 04.Fill-Dropdown

--- JS_Advanced/DOM-manipulation/exercise/04. Fill-Dropdown/dropdown.js
+++ JS_Advanced/DOM-manipulation/exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemTextElement = document.getElementById('newItemText');
+    let itemValueElement = document.getElementById('newItemValue');
+    let selectElement = document.getElementById('menu');
+
+    let optionsElement = document.createElement('option');
+    
+    if (itemTextElement.value && itemValueElement.value) {
+        optionsElement.textContent = itemTextElement.value;
+        optionsElement.value = itemValueElement.value;
+        selectElement.appendChild(optionsElement);
+        itemTextElement.value = '';
+        itemValueElement.value = '';
+    }
 }
