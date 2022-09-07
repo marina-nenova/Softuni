f090f7e8ecada5226a4b5d9d564b81aecc11b5e3
Marina Nenova <marinanenova89@gmail.com>
2022-09-07 21:47:36 +0300

    Added logic for 07.Distance-Converter

--- JS_Advanced/DOM-manipulation/exercise/07. Distance-Converter/distanceConverter.js
+++ JS_Advanced/DOM-manipulation/exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,31 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let [inputField, outputField] = document.querySelectorAll('input[type="text"]');
+    let buttonElement = document.getElementById('convert');
+
+    let inputUnits = document.getElementById('inputUnits');
+    let outputUnits = document.getElementById('outputUnits');
+
+    let rations = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    }
+
+    function convertUnits() {
+        let convertValue = Number(inputField.value);
+        let fromValue = inputUnits.value;
+        let toValue = outputUnits.value;
+        let convertValueInMeters = convertValue * rations[fromValue];
+
+        result = convertValueInMeters / rations[toValue];
+        outputField.value = result;
+    }
+
+    buttonElement.addEventListener('click', convertUnits);
 }
+
