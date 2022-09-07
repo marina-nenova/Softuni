1b5a3fee4e66dd776d7255e0b547ec507ca1689a
Marina Nenova <marinanenova89@gmail.com>
2022-09-07 09:10:02 +0300

    Added logic for 01.Sections

--- JS_Advanced/DOM-manipulation/exercise/01. Sections/script.js
+++ JS_Advanced/DOM-manipulation/exercise/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   let contentElement = document.getElementById('content');
+
+   words.forEach(element => {
+      let pElement = document.createElement('p');
+      pElement.style.display = 'none';
+      let divElement = document.createElement('div');
+
+      pElement.textContent = element;
+      divElement.appendChild(pElement);
+
+      divElement.addEventListener('click', () => {
+         pElement.style.removeProperty('display');
+      })
+
+      contentElement.appendChild(divElement);
+   });
 }
