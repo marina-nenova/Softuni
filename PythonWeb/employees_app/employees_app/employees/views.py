2759c10b24df9cb3ee74792fb434dd5daf47e07a
Marina Nenova <marinanenova89@gmail.com>
2022-09-21 14:45:04 +0300

    Created two views - home and department

--- PythonWeb/employees_app/employees_app/employees/views.py
+++ PythonWeb/employees_app/employees_app/employees/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return HttpResponse('This is home')
+
+def department(request):
+    return HttpResponse('This is department')
