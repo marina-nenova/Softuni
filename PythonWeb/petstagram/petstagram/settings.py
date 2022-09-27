2d682f3f43334fd663a3d376fa500983344c268e
Marina Nenova <marinanenova89@gmail.com>
2022-09-27 12:54:01 +0300

    Added static files and templates to each app. Also added the apps in the installed apps list in settings.py and set the STATICFILE_DIRS in settings.py

--- PythonWeb/petstagram/petstagram/settings.py
+++ PythonWeb/petstagram/petstagram/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project apps
+    'petstagram.accounts',
+    'petstagram.common',
+    'petstagram.pets',
+    'petstagram.photos',
+
 ]
 
 MIDDLEWARE = [
@@ -117,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
