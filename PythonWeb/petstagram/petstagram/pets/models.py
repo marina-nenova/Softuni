41e480f72d2620712981ab30e2cbe91c84b12e7c
Marina Nenova <marinanenova89@gmail.com>
2022-10-03 13:33:52 +0300

    Added pet model and registerred admin

--- PythonWeb/petstagram/petstagram/pets/models.py
+++ PythonWeb/petstagram/petstagram/pets/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30
+    )
+    personal_photo = models.URLField()
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True
+    )
+    slug = models.SlugField(
+        unique=True,
+        editable=False,
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+        return super().save(*args, **kwargs)
