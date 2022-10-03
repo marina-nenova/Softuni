71309633580ff055173052c5445017cc0090eb80
Marina Nenova <marinanenova89@gmail.com>
2022-10-03 14:10:32 +0300

    Added comment and like models

--- PythonWeb/petstagram/petstagram/common/admin.py
+++ PythonWeb/petstagram/petstagram/common/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from petstagram.common.models import Comment, Like
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['comment_text', 'date_time_of_publication', 'to_photo']
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['to_photo']
+
--- PythonWeb/petstagram/petstagram/common/models.py
+++ PythonWeb/petstagram/petstagram/common/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from petstagram.photos.models import Photo
+
+
+class Comment(models.Model):
+    comment_text = models.TextField(
+        max_length=300,
+    )
+    date_time_of_publication = models.DateField(auto_now_add=True)
+    to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
